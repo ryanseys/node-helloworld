8dea8673f904e7eedf281d3e525780b1cd159654
Ryan Seys <ryan@ryanseys.com>
2013-10-26 16:10:04 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ app.get('/', function (req, res) {
   res.send('Hello World\n');
 });
 
-app.listen(PORT, 'localhost');
+app.listen(PORT, '127.0.0.1');
 console.log('Running on http://localhost:' + PORT);
